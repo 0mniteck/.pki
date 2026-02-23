f8f90b46f056a6c16935aa7b0e826ebdd4a78028
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-22 19:37:23 -0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env -S - bash --norc --noprofile
+
 fetch.pki() { # $1 = domain/FQDN
-pushd registry
+mkdir -p local
+pushd local
   openssl s_client -servername $1 -connect $1:443 < /dev/null | sed -n "/-----BEGIN/,/-----END/p" > $1.pem && \
-  openssl x509 -in $1.pem -pubkey -noout > $1.pubkey.pem && openssl x509 -in $1.pem -enddate -noout > $1.exp &&\
+  openssl x509 -in $1.pem -pubkey -noout > $1.pubkey.pem && openssl x509 -in $1.pem -enddate -noout > $1.exp && \
   openssl asn1parse -noout -inform pem -in $1.pubkey.pem -out $1.pubkey.der && \
   openssl dgst -sha256 -binary $1.pubkey.der | openssl base64 > $1.pubkey && \
   rm -f *.pem *.der || exit 1
   echo "Successfully fetched pubkey for $1"
 popd
+}
+
+check.remote.pki() { # $1 = domain/FQDN
+  curl -o test/$1.pubkey -s --pinnedpubkey "sha256//$(<registry/www.cloudflare.com.pubkey)" \
+--tlsv1.3 --proto -all,+https --remove-on-error --no-insecure https://www.cloudflare.com/cdn-cgi/trace
 }
 
 check.csv() { # $1 = domain/FQDN
@@ -16,6 +23,15 @@ check.csv() { # $1 = domain/FQDN
   if [[ "$dater" -le "$date" ]]; then
     fetch.pki $1
   fi
+}
+
+check.against.csv() { # $1 = domain/FQDN
+  dater=$(date -d "$(cat local/$1.exp | cut -d'=' -f2)" +%s)
+  date=$(date +%s)
+  if [[ "$dater" -le "$date" ]]; then
+    fetch.pki $1
+  fi
+  check.remote.pki $1
 }
 
 check.pki() { # $1 = domain/FQDN
@@ -27,7 +43,14 @@ check.pki() { # $1 = domain/FQDN
 }
 
 invalidate.pki() {
+  rm -f local/$1.pubkey.valid test/$1.pubkey
+  cp local/$1.pubkey local/$1.pubkey.invalid
+}
+
+validate.pki() {
+  if [[ ]]; then
   
+  rm -f local/$1.pubkey local/$1.pubkey.invalid
 }
 
 check.against.pki() { # $1 = domain/FQDN
@@ -51,5 +74,7 @@ check.against.index() {
 }
 
 check.index
+check.liveness
+check.against.index
 
 echo DONE
